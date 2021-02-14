ede86cabc024bd07c28dbfe3668f59ac9a153668
Shahed Ahmad <shaheda.a16@gmail.com>
2021-02-14 17:00:00 +0430

    loading no-result ref.focus and styles added

--- src/components/main.js
+++ src/components/main.js
@@ -3,14 +3,17 @@ import {Search} from './search'
 
 class main extends Component {
     constructor(props) {
-        super(props)
-    
+        super(props);
+        this.inputRef = React.createRef();
         this.state = {
-             data: false
+
         }
     }
 
     search = async(value) =>{
+        this.setState({
+            data: false
+        })
         let data = await fetch(`https://pixabay.com/api/?key=20223274-42276db0604df23b312b7ef2f&q=${value}&image_type=photo`);
         data = await data.json()
         this.setState({
@@ -26,11 +29,14 @@ class main extends Component {
     }
     
     render() {
-        let wrap = "place-content-start place-items-center grid grid-cols-8 gap-2 col-start-1 col-end-13 pt-10"
+        let wrap = "place-content-start place-items-center grid grid-cols-8 gap-2 col-start-1 col-end-13 pt-10";
+        let load = "col-start-1 col-end-9 justify-center text-gray-300 mt-48 font-semibold text-3xl";
+        let noResult = Array.isArray(this.state.data) && this.state.data == false;
         return (
             <div className="w-full m-0 p-2 grid grid-cols-12 justify-center">
-                <Search search={this.search}/>
+                <Search search={this.search} inputRef={this.inputRef}/>
                 <div className={wrap}>
+                    {this.state.data === false && <p className={load}>Loading...</p>}
                     {this.state.data && this.state.data.map(item => {
                         let imageDiv = this.imageSize(item.previewHeight)
                         return (
@@ -39,6 +45,7 @@ class main extends Component {
                             </div>
                         )
                     })}
+                    {noResult && <p className={load}>No Result</p>}
                 </div>
                 
             </div>
--- src/components/search.js
+++ src/components/search.js
@@ -9,6 +9,10 @@ class Search extends Component {
         }
     }
 
+    componentDidMount(){
+        this.props.inputRef.current.focus()
+    }
+
     write = (event) => {
         this.setState({
             value: event.target.value
@@ -18,7 +22,7 @@ class Search extends Component {
     render() {
         return (
             <div className="col-start-1 col-end-13 text-center pt-4">
-                <input type="search" value={this.state.value} onChange={this.write} className='border-2 p-1 pr-24 w-1/3 rounded outline-none focus:border-gray-500'/>
+                <input type="search" value={this.state.value} onChange={this.write} ref={this.props.inputRef} className='border-2 p-1 pr-24 w-1/3 rounded outline-none focus:border-gray-500'/>
                 <button className="bg-yellow-300 py-1.5 outline-none px-6 font-semibold -ml-24 rounded-r hover:bg-yellow-400" onClick={()=> this.props.search(this.state.value)}>Search</button>
             </div>
         )
