176d07240d37e84cb4d5bd7b9a943a0346bdbb69
Shahed Ahmad <shaheda.a16@gmail.com>
2021-04-30 23:18:48 +0430

    perfect check button made

--- src/components/theme_changer_checkbox.js
+++ src/components/theme_changer_checkbox.js
@@ -3,7 +3,9 @@ import React from 'react'
 function ThemeCheckbox() {
     return (
         <div>
-            <label className="w-3 h-1 bg-gray-200 rounded-full" for="check"></label>
+            <label className="w-10 h-5 bg-gray-200 rounded-full absolute inset-6 pointer" for="check">
+                <div className="w-6 h-6 bg-black rounded-full absolute -inset-x-1 -inset-y-0.5"></div>
+            </label>
             <input type='checkbox' id="check" className="hidden" />
         </div>
     )
