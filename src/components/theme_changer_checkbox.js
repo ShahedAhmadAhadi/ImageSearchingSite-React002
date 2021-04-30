7e65dd538ba9986e5b113102befa75a215938ccc
Shahed Ahmad <shaheda.a16@gmail.com>
2021-04-30 23:39:23 +0430

    render bug in ThemeChanger

--- src/components/theme_changer_checkbox.js
+++ src/components/theme_changer_checkbox.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export class ThemeCheckbox extends Component {
     render() {
         return (
-            <div onClick={() => this.props.theme()}>
-                <label className="w-10 h-5 bg-gray-200 rounded-full absolute inset-6 pointer" htmlFor="check">
+            <div>
+                <label onClick={() => this.props.theme()} className="w-10 h-5 bg-gray-200 rounded-full absolute inset-6 pointer" htmlFor="check">
                 <div className="w-6 h-6 bg-black rounded-full absolute -inset-x-1 -inset-y-0.5"></div>
                 </label>
                 <input type='checkbox' id="check" className="hidden" />
