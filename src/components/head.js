0a112973eccb800160b562d447ca9744a9012d49
Shahed Ahmad <shaheda.a16@gmail.com>
2021-05-01 09:43:06 +0430

    refactor

--- src/components/head.js
+++ src/components/head.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 function head(props) {
-    let theme_value = {
-        
+    let themeValue = {
+        textColor: 'yellow-800',
+        bgOpaciry: '80'
     }
     return (
-        <header className="p-4 md:w-80 relative text-center h-16 cursor-pointer hover:h-48 overflow-hidden w-full font-bold text-white float-left text-xl">
+        <header className={`p-4 md:w-80 relative text-center h-16 cursor-pointer hover:h-48 overflow-hidden w-full font-bold text-${props.theme ? 'white' : themeValue.textColor} bg-opacity-${props.theme ? '100' : themeValue.bgOpaciry} float-left text-xl`}>
             <i className="bi bi-images mx-2 text-2xl"></i>
             <span className={``}>GALLERY
             {/* <div className="absolute inset-0 top-16 w-48 m-auto bg-white text-black">
