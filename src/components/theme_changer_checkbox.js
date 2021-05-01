43fe4ba8ebe1364893a5c1b617821fd0007b5f3c
Shahed Ahmad <shaheda.a16@gmail.com>
2021-05-01 10:11:03 +0430

    themeChanger checkBox completed

--- src/components/theme_changer_checkbox.js
+++ src/components/theme_changer_checkbox.js
@@ -22,9 +22,9 @@ class ThemeCheckbox extends Component {
     render() {
         return (
             <div>
-                <label onClick={() => {this.props.themeChanger(); this.updateTheme()}} className="w-10 h-5 bg-gray-200 rounded-full absolute inset-6 pointer" htmlFor="check">
-                    <div className={`w-6 h-6 bg-black rounded-full absolute ${this.state.theme ? '-intset-x-0' : 'inset-x-4'} -inset-y-0.5`}>
-                        <i className={`bi bi-${this.state.theme ? 'brightness-high' : 'moon'} text-white absolute -inset-y-0.5 inset-x-0.5`}></i>
+                <label onClick={() => {this.props.themeChanger(); this.updateTheme()}} className={`bg-${this.state.theme ? 'indigo-900' : 'purple-50'} w-10 h-5 rounded-full absolute inset-6 pointer`} htmlFor="check">
+                    <div className={`w-6 h-6 bg-${this.state.theme ? 'purple-50' : 'black'}  rounded-full absolute ${this.state.theme ? '-intset-x-0' : 'inset-x-4'} -inset-y-0.5`}>
+                        <i className={`bi bi-${this.state.theme ? 'moon' : 'brightness-high'}  text-${this.state.theme ? 'black' : 'white'} absolute -inset-y-0.5 inset-x-1 `}></i>
                     </div>
                 </label>
                 <input type='checkbox' id="check" className="hidden" />
