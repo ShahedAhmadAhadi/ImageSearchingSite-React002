340bdd8d451941c9dc4b5f68089bcb37f3203989
Shahed Ahmad <shaheda.a16@gmail.com>
2021-05-01 09:32:16 +0430

    refactor

--- src/components/search.js
+++ src/components/search.js
@@ -28,14 +28,17 @@ class Search extends Component {
     
     render() {
         console.log(this.props)
-        let theme_value = {
-            input_background: 'indigo-900',
-            input_TextColor: 'white'
+        let themeValue = {
+            inputBackground: 'indigo-900',
+            inputTextColor: 'white',
+            searchButtonBackground: 'red-800',
+            searchButtonOpaciry: '80',
+            searchButtonColor: 'white'
         }
         return (
             <div className={`col-start-1 col-end-13 sm:text-center box-border p-4`}>
-                <input type="search" value={this.state.value} onChange={this.write} onKeyPress={this.enter} ref={this.props.inputRef} className={`border-2 m-0 p-1 pr-24 bg-${!this.props.theme ? theme_value.input_background : 'white'} text-${!this.props.theme ? theme_value.input_TextColor : 'black'} lg:w-1/3 md:w-2/4 sm:w-3/4 w-full rounded outline-none focus:border-gray-500`}/>
-                <button className="bg-yellow-300 absolute py-1.5 px-6 font-semibold -ml-24 rounded-r hover:bg-yellow-400" onClick={()=> this.props.search(this.state.value)}>Search</button>
+                <input type="search" value={this.state.value} onChange={this.write} onKeyPress={this.enter} ref={this.props.inputRef} className={`border-2 m-0 border-green-600 p-1 pr-24 bg-${!this.props.theme ? themeValue.inputBackground : 'white'} text-${!this.props.theme ? themeValue.inputTextColor : 'black'} lg:w-1/3 md:w-2/4 sm:w-3/4 w-full rounded outline-none focus:border-gray-500`}/>
+                <button className={`bg-${this.props.theme ? 'yellow-300' : themeValue.searchButtonBackground} bg-opacity-${this.props.theme ? '100' : themeValue.searchButtonOpaciry} absolute py-1.5 px-6 font-semibold text-${this.props.theme ? 'black' : themeValue.searchButtonColor} -ml-24 rounded-r hover:bg-yellow-400`} onClick={()=> this.props.search(this.state.value)}>Search</button>
             </div>
         )
     }
